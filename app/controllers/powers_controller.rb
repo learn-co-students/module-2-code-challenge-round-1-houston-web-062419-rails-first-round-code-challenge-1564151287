b00bcd726fe47f43e1828f05e04f1a0c558575a8
Aaron Briscoe <gmail2008@gmail.com>
2019-07-26 10:24:12 -0500

    done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,17 @@
 class PowersController < ApplicationController
+
+  before_action :current_power, only: [:show, :edit, :update, :destroy]
   def index
     @powers = Power.all
   end
+
+  def show
+  end
+
+  private
+  def current_power
+    @power = Power.find(params[:id])
+  end
+
+
 end
