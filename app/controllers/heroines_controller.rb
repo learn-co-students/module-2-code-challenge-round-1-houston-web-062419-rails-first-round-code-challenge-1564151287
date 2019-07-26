aeaea0abd214077ef30337744f99fadd0ad03aeb
Varvara Babkova <varvara.munday@gmail.com>
2019-07-26 10:09:04 -0500

    first version

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,30 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+  	@heroine = Heroine.find(params[:id])
+  	@power = @heroine.power
+  end
+
+  def new
+  	@heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(h_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+
+  def h_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
